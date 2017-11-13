83d3c7f26b345f1694326e038ff378e6c149c56b
Benedict Wee <benedictwee.2014@sis.smu.edu.sg>
2017-11-13 23:31:51 +0800

    Update testing for skills of employees

--- optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/testing/WorkerRosteringTest.java
+++ optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/testing/WorkerRosteringTest.java
@@ -2,7 +2,10 @@ package org.optaplanner.training.workerrostering.testing;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -29,10 +32,16 @@ import org.optaplanner.training.workerrostering.persistence.WorkerRosteringSolut
 public class WorkerRosteringTest {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         
-        String FILE_NAME = "/Users/ben/Downloads/roster-34spots-14days-solved20171113201119.xlsx";
+        //String FILE_NAME = "/Users/ben/Downloads/roster-34spots-14days-solved20171113201119.xlsx";
+        String FILE_NAME = "/Users/ben/Downloads/roster-34spots-14days-solved20171113214315.xlsx";
+        
+        String[] skills = {"Malay", "Tamil", "Mandain"};
+        ArrayList<String> currentWorkers = new ArrayList<>();
+        HashMap<String, String> schedule = new HashMap<>();
+        
+        
         
         try {
-
             FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
             Workbook workbook = new XSSFWorkbook(excelFile);
             Sheet datatypeSheet = workbook.getSheetAt(0);
@@ -58,30 +67,19 @@ public class WorkerRosteringTest {
                         }*/
                         
                         row += currentCell.getStringCellValue() + ",";
-                        //arr.add(row);
-                        //System.out.print(currentCell.getStringCellValue());
                 }
-                //System.out.println(row);
+        
                 result.add(row);
-                //System.out.print(arr.get(0));
-                //System.out.println();
-
             }
             
             
             int checker = 0;
             int checker2 = 0;
             for (int counter = 2; counter < result.size(); counter++) { 
-                //int length = result.get(2).length();
-                //String worker = result.get(2).substring(0,length-2);
                 int length = result.get(counter).length();
                 String worker = result.get(counter).substring(0,length-2);
-                System.out.println(worker);
+                //'System.out.println(worker);
                 String[] workerSplit = worker.split(",");
-                //System.out.println(workerSplit[0]);
-                //System.out.println(workerSplit.length);
-                
-                //String firstTiming = workerSplit[1];
                 String status = "";
                 for(int count = 1; count < workerSplit.length; count++){
                     String work = workerSplit[count];
@@ -91,21 +89,50 @@ public class WorkerRosteringTest {
                         status += "0,";
                     }else{
                         status += "1,";
-                    }
-                    
+                    } 
                 }
                 
                 
         
                 int consecutive = 0;
-                //System.out.println(status);
                 status = status.substring(0, (status.length()-1) );
-                //System.out.println(status);
+                
+                /*if(status.indexOf("1") != -1){
+                    currentWorkers.add(workerSplit[0]);
+                }*/
+                
+                
                 String[] status_arr = status.split(",");
+                
+                
+                
+               
+                System.out.println(workerSplit[0]);
+                int dayCounter = 0;
+                for(int i = 0; i < status_arr.length -1; i++){
+                    String work = status_arr[i];
+                    //System.out.println(work);
+                    String day = "" + dayCounter;
+                    if(work.equals("1")){
+                        if(schedule.get(day) == null){
+
+                            ArrayList<String> arr = new ArrayList<>();
+                            schedule.put(day, workerSplit[0]+",");
+                        }else{
+                            String workers = schedule.get(day);
+                            workers += workerSplit[0]+",";
+                            schedule.put(day, workers);
+
+                        }
+                    }
+                    dayCounter++;
+                }
+                //System.out.println("S " + schedule.get("2"));
                 
                 String currentStatus = status_arr[0];
                 for(int statusCount =1; statusCount < status_arr.length - 1; statusCount++){
                     String work_status = status_arr[statusCount];
+                    //System.out.println(work_status);
                     String upcoming_status = status_arr[statusCount];
 
                     //FIRST CHECK ERROR - 2 Work back to back
@@ -131,9 +158,86 @@ public class WorkerRosteringTest {
                     }
                 } 
             }
+           
+            
             System.out.println("BACK BACK SHIFT ERRORS " + checker);
             System.out.println("CONSECUTIVE WORK BUT NO 5 DAYS REST - ERRORS " + checker2);
-            //} 
+            
+            
+            
+            ArrayList<String> workerList = new ArrayList<String>();
+            datatypeSheet = workbook.getSheetAt(2);
+            iterator = datatypeSheet.iterator();
+            while (iterator.hasNext()) {
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+                
+                String row = "";
+                while (cellIterator.hasNext()) {
+                    Cell currentCell = cellIterator.next();
+                    //System.out.println(currentCell.getCellType());
+                    if(currentCell.getCellType() == 1){
+                        row += currentCell.getStringCellValue() + ",";
+                    }
+                }
+                workerList.add(row);
+            }
+            
+            
+            
+            
+            
+            Iterator it = schedule.entrySet().iterator();
+            
+                
+                while (it.hasNext()) {
+                    Map.Entry pair = (Map.Entry)it.next();
+                    System.out.println(pair.getKey() + " = " + pair.getValue());
+                    
+                    String value = ""+pair.getValue();
+                    String[] valuearr = value.split(",");
+                    
+                    int chineseCheck = 0;
+                    int malayCheck = 0;
+                    int tamilCheck = 0;
+                    for (int counter = 2; counter < workerList.size(); counter++) { 
+                        //System.out.println(workerList.get(counter));
+                        String worker = workerList.get(counter);
+                        for (int i = 0; i < valuearr.length; i++) { 
+                            String shiftWorker = valuearr[i];
+                            //System.out.println(shiftWorker);
+
+                            if(worker.indexOf(shiftWorker) != -1){
+                                if(worker.indexOf("Malay") != -1){
+                                    malayCheck++;
+                                }
+                                if(worker.indexOf("Tamil") != -1){
+                                    tamilCheck++;
+                                }
+                                if(worker.indexOf("Mandarin") != -1){
+                                    chineseCheck++;
+                                }
+                            }
+                        }
+                    }
+                    System.out.println(pair.getKey() + " = CHINESE - " + chineseCheck + " MALAY - " + malayCheck + " TAMIL - " + tamilCheck );
+                    it.remove(); // avoids a ConcurrentModificationException
+                }
+                
+                
+            
+            
+    
+            
+            /*if(chineseCheck > 0 && malayCheck > 0 && tamilCheck > 0){
+                System.out.println("SKILLS TEST CASE PASS - CHINESE - " + chineseCheck + " MALAY - " + malayCheck + " TAMIL - " + tamilCheck );
+            }else{
+                System.out.println("SKILLS TEST CASE FAIL - CHINESE - " + chineseCheck + " MALAY - " + malayCheck + " TAMIL - " + tamilCheck );
+            }*/
+            
+            
+            
+            //System.out.println(currentWorkers.get(0));
             
         } catch (FileNotFoundException e) {
             e.printStackTrace();
